10d51c8f6c07f00e65d416755c85a8bf7e7a9d1a
Steve Betts <sbetts@minethurn.com>
2026-06-28 23:56:56 +0000

    initial readme and license

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # dmbc-extras
+
+A wordpress plugin that adds in functionality to bind other plugins together and perform functions necessary to running the chorus.
+
+Like:
+
+1. email all the members
+1. host learning tracks
+1. sell tickets to events
+1. host misc pages
---
+++ dmbc-extras.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name:       DMBC Extras
+ * Plugin URI:        https://daytonmetrobarbershopchorus.com
+ * Description:       Adds in functionality to bind other plugins together
+ *                    and perform functions necessary to running the chorus.
+ * Version:           1.0.0
+ * Author:            Steve Betts
+ * Author URI:        https://github.com/zagrev
+ * License:           CC BY-NC-ND
+ * Text Domain:       dmbc-extras
+ *
+* dmbc-extras is free software: you can redistribute it and/or modify
+* it under the terms of the CC-BY-NC-ND but you cannot make or sell
+* a commercial product or derivitive commercial product using this code.
+* See https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode.txt
+* 
+* dmbc-extras is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* 
+*/
+ */
+
+// Security Check: Prevent direct file access outside of WordPress
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
